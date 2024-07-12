52bb36484426dc3ad94cf81903d38a2f76d196d3
DinarRikmaBara <rikmabara@gmail.com>
2024-07-13 02:11:15 +0700

    Sidebar

---
+++ src/Pages/Dashboard.jsx
@@ -0,0 +1,16 @@
+import { Sidebar } from "../Layout/Sidebar"
+
+
+
+export const Dashboard = () => {
+    return (
+        <div className="flex">
+            <div className="flex-initial">
+            <Sidebar></Sidebar>
+            </div>
+            <div className="flex-initial">
+                haloo
+            </div>
+        </div>
+    )
+}
